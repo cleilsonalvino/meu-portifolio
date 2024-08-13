da1ae20aa14feadf26f5c3b7141585098107d36d
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2024-08-13 00:11:24 -0300

    new configs

---
+++ script.js
@@ -0,0 +1,7 @@
+const projects = document.getElementById("projects");
+
+projects.addEventListener("click", () => {
+  setTimeout(() => {
+    window.location.href = "https://github.com/cleilsonalvino?tab=repositories";
+  }, 1000);
+});
